58908e9a1f1a81a183e2fa0b4c17bffca8403e39
Andrius Alimas <andriusalimaswebdeveloper@gmail.com>
2022-04-22 12:54:48 +0100

    Finish project

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as ObjectDetectorContainer } from "./objectDetector/ObjectDetector.element";
+export { default as DetectorContainer } from "./objectDetector/ObjectDetector.element";
+export { default as TargetImg } from "./objectDetector/ObjectDetector.element";
+export { default as HiddenFileInput } from "./objectDetector/ObjectDetector.element";
+export { default as SelectButton } from "./objectDetector/ObjectDetector.element";
---
+++ src/components/objectDetector/ObjectDetector.component.jsx
@@ -0,0 +1,106 @@
+import React, { useRef, useState } from "react";
+import {
+  ObjectDetectorContainer,
+  DetectorContainer,
+  HiddenFileInput,
+  SelectButton,
+  TargetImg,
+  TargetBox,
+} from "./ObjectDetector.element";
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+
+export function ObjectDetector(props) {
+  const [imgData, setImgData] = useState(null);
+  const [predictions, setPredictions] = useState([]);
+  const fileInputRef = useRef();
+  const imageRef = useRef();
+  const isEmptyPredictions = !predictions || predictions.length === 0;
+
+  const openFilePicker = () => {
+    if (fileInputRef.current) fileInputRef.current.click();
+  };
+
+  const normalizePredictions = (predictions, imageSize) => {
+    if (!predictions || !imageSize || !imageRef) return predictions || [];
+    return predictions.map((prediction) => {
+      const { bbox } = prediction;
+      const oldX = bbox[0];
+      const oldY = bbox[1];
+      const oldWidth = bbox[2];
+      const oldHeight = bbox[3];
+
+      const imgWidth = imageRef.current.width;
+      const imgHeight = imageRef.current.height;
+
+      const x = (oldX * imgWidth) / imageSize.width;
+      const y = (oldY * imgHeight) / imageSize.height;
+      const width = (oldWidth * imgWidth) / imageSize.width;
+      const height = (oldHeight * imgHeight) / imageSize.height;
+
+      return { ...prediction, bbox: [x, y, width, height] };
+    });
+  };
+
+  const detectObjectsOnImage = async (imageElement, imageSize) => {
+    const model = await cocoSsd.load({});
+    const predictions = await model.detect(imageElement, 6);
+    const normalizedPredictions = normalizePredictions(predictions, imageSize);
+    setPredictions(normalizedPredictions);
+    console.log("Predictions: ", predictions);
+  };
+
+  const readImage = (file) => {
+    return new Promise((rs, rj) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => rs(fileReader.result);
+      fileReader.onerror = () => rj(fileReader.error);
+      fileReader.readAsDataURL(file);
+    });
+  };
+
+  const onSelectImage = async (e) => {
+    // select only one single file
+    const file = e.target.files[0];
+    const imgData = await readImage(file);
+    setImgData(imgData);
+
+    const imageElement = document.createElement("img");
+    imageElement.src = imgData;
+
+    imageElement.onload = async () => {
+      const imgSize = {
+        width: imageElement.width,
+        height: imageElement.height,
+      };
+      await detectObjectsOnImage(imageElement, imgSize);
+    };
+  };
+
+  return (
+    <ObjectDetectorContainer>
+      <DetectorContainer>
+        {imgData && <TargetImg src={imgData} ref={imageRef} />}
+        {!isEmptyPredictions &&
+          predictions.map((prediction, index) => (
+            <TargetBox
+              key={index}
+              x={prediction.bbox[0]}
+              y={prediction.bbox[1]}
+              width={prediction.bbox[2]}
+              height={prediction.bbox[3]}
+              classType={prediction.class}
+              score={prediction.score * 100}
+            />
+          ))}
+      </DetectorContainer>
+      <HiddenFileInput
+        type="file"
+        ref={fileInputRef}
+        onChange={onSelectImage}
+      />
+      <SelectButton onClick={openFilePicker}>Select Image</SelectButton>
+    </ObjectDetectorContainer>
+  );
+}
