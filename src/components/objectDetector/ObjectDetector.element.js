6729f6aa130363c00af3aac8cbfc657bd03ce595
Andrius Alimas <andriusalimaswebdeveloper@gmail.com>
2022-04-22 13:35:00 +0100

    add procentage sign on prediction to look nicer

--- src/components/objectDetector/ObjectDetector.element.js
+++ src/components/objectDetector/ObjectDetector.element.js
@@ -50,7 +50,7 @@ export const TargetBox = styled.div`
   background-color: transparent;
   z-index: 20;
   &::before {
-    content: "${({ classType, score }) => `${classType} ${score.toFixed(2)}%`}";
+    content: "${({ classType, score }) => `${classType} ${score.toFixed(1)}%`}";
     color: #1ac71a;
     font-weight: 500;
     font-size: 17px;
